70c8e22eb2d511e9d01e0518427e4ec391d68276
muhammaddariazzidane <dariazzidane@gmail.com>
2022-09-17 21:02:01 +0700

    deploy 6

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="absolute left-0 right-0 top-[17%] lg:top-[33%] flex  flex-col items-center justify-center flex-1 text-center">
+    <div className="absolute left-0 right-0 top-[20%] lg:top-[33%] flex  flex-col items-center justify-center flex-1 text-center">
       <h1 className="lg:text-5xl text-2xl font-bold text-white max-w-lg lg:px-0 px-12">Unlimited movies, TV shows, and more.</h1>
       <h2 className="lg:text-2xl text-lg mb-3 lg:mb-6 my-5 text-white ">Watch anywhere. Cancel anytime.</h2>
       <p className="max-w-xs lg:max-w-full text-white">Ready to watch? Enter your email to create or restart your membership.</p>
